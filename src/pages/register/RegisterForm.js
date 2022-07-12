b03fdd1115aa615c9508f3083d6c2071f382f896
Oscar Reyes <oscarreyes@Oscars-MBP.home>
2022-07-13 00:18:28 +0200

    Register form works but doesn't validate, pendint to fix

--- src/pages/register/RegisterForm.js
+++ src/pages/register/RegisterForm.js
@@ -1,81 +1,104 @@
-import React from 'react'
+import { React, useState, useRef } from 'react'
 import './index.css'
 import bgImg from '../../assets/img11.jpg'
 import { useForm } from 'react-hook-form'
 
 import { Navigate, useNavigate } from 'react-router-dom'
-import { hasUserSession, setUserSession } from '../../utils/sesion'
+import { setUserSession } from '../../utils/sesion'
+import api from '../../utils/api'
 
 const RegisterForm = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm()
+  const { register, handleSubmit } = useForm()
+
+  const errRef = useRef()
+  const [errMsg, setErrMsg] = useState('')
 
   //* This is to navigate to a page of our choice through router
   const navigate = useNavigate()
 
-  const authRegister = ({ password, passwordConfirm }) => {
-    !password === passwordConfirm ? Navigate('/register') : Navigate('/')
+  //* This is to avoid the reload of the page
+  const onSubmit = (data) => {
+    // e.preventDefault()
+
+    const { name, email, password } = data
+    const authRegister = ({ password, passwordConfirm }) => {
+      !password === passwordConfirm ? Navigate('/register') : Navigate('/')
+    }
+    const body = { name, email, password }
+    api('POST', 'register', { body })
+      .then((userSession) => {
+        setUserSession(userSession)
+        navigate('/home')
+      })
+      .catch((e) => {
+        setErrMsg(e.message)
+        console.error(e)
+      })
   }
+
   //   console.log(watch('name'))
 
   return (
     //Add parent component container
     <section>
-      {hasUserSession() && <Navigate to='/home' replace />}
-      {!hasUserSession() && (
-        <div className='register-form'>
-          <div className='col-1'>
-            <h2>Sign In</h2>
-            <span>register and enjoy the service</span>
-            <form
-              id='form'
-              className='flex flex-col'
-              onSubmit={handleSubmit(authRegister)}
-            >
-              <input
-                {...register('name', { required: true })}
-                type='text'
-                placeholder='Enter your name'
-              />
-              <input
-                {...register('email', { required: true })}
-                type='email'
-                placeholder='Email address'
-              />
-              <input
-                {...register('password', { required: true })}
-                type='password'
-                placeholder='Enter password'
-              />
-              <input
-                {...register('passwordConfirm', { required: true })}
-                type='password'
-                placeholder='Confirm your password'
-              />
-              <button className='btn'>Sign In</button>
-              {/*Redirect to log in, in case of having and account*/}
-              <p>
-                Already have an account?
-                <span className='line'>
-                  <a
-                    style={{ cursor: 'pointer' }}
-                    onClick={() => navigate('/login')}
-                  >
-                    Sign In!
-                  </a>
-                </span>
-              </p>
-            </form>
-          </div>
-          <div className='col-2'>
-            <img src={bgImg} alt='' />
-          </div>
+      <div className='register-form'>
+        <div className='col-1'>
+          <span
+            ref={errRef}
+            className={errMsg ? 'errmsg' : 'offscreen'}
+            aria-live='assertive'
+          >
+            {errMsg}
+          </span>
+          <h2>Sign In</h2>
+          <span>register and enjoy the service</span>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            id='form'
+            className='flex flex-col'
+          >
+            <input
+              {...register('name', { required: true })}
+              type='text'
+              placeholder='Enter your name'
+            />
+            <input
+              {...register('email', { required: true })}
+              type='email'
+              placeholder='Enter email address'
+            />
+
+            <input
+              {...register('password', { required: true })}
+              type='password'
+              placeholder='Enter password'
+            />
+            <input
+              {...register('passwordConfirm', {
+                required: true,
+              })}
+              type='password'
+              placeholder='Confirm your password'
+            />
+            <button className='btn'>Sign In</button>
+            {/*Redirect to log in, in case of having and account*/}
+            <span>
+              Already have an account?
+              <span className='line'>
+                <a
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => navigate('/login')}
+                >
+                  Sign In!
+                </a>
+              </span>
+            </span>
+          </form>
         </div>
-      )}
+        <div className='col-2'>
+          <img src={bgImg} alt='' />
+        </div>
+      </div>
     </section>
   )
 }
