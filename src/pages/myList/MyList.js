4ea212d2b08986bf357a4e89c1c9c3e48a658c91
Noemi <noemipb7@gmail.com>
2022-07-19 16:44:55 +0200

    creo llamada a la api con los favs

--- src/pages/myList/MyList.js
+++ src/pages/myList/MyList.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Card from '../../components/card/Card';
+import api from '../../utils/api';
 
 const MyList = () => {
-  return <div>LIST</div>
-}
 
+    const [favList, setFavlist] = useState ([]);
+    const [nameMovieFav, setMovieFav] = useState([]);
+
+    useEffect(() => {
+        api('GET', 'favs').then((favs) => setFavlist(favs));
+    }, [favList]);
+
+    useEffect(() => {
+        api('GET', 'movies')
+    }, [])
+        
+  return (
+  <div>Lista de favoritos 
+    {favList && favList.map(({_id}) => (
+        <Card
+        id = {_id}
+        />
+    ))}
+    
+ </div>
+)
+}
 export default MyList
