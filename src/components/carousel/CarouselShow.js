31940ee88d2bb9deee8ea51e36fccae10d2717e2
insert-oreyes <oreyes.s@outlook.com>
2022-09-03 14:26:24 +0200

    Fix carousel modal to show

--- src/components/carousel/CarouselShow.js
+++ src/components/carousel/CarouselShow.js
@@ -1,13 +1,17 @@
 import { React, useRef, useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import './carouselShow.css';
-import Card from '../card/Card';
-import '../card/Card.css';
+import Modal from '../modal/Modal';
+import { getFavMoviesFromLocalStorage, getUserSession } from '../../utils/sesion';
+import { getFavMovies, movieToFav, removeMovieFromFav } from '../../utils/movies';
 
 const CarouselShow = (props) => {
-  const { movies } = props;
+  const { movies, urlImgMovie, title, urlImgModal, movieDescription, movieRating, movieRuntime, id } = props;
   const [width, setWidth] = useState(0);
-  const [refresh, setRefresh] = useState(false);
+  //! This is for check!
+  const [modalOpen, setModalOpen] = useState(false);
+  const [fav, setFav] = useState(false);
+
   // useRef is kinda document.queryselector in JS
   const carousel = useRef();
   useEffect(() => {
@@ -18,30 +22,55 @@ const CarouselShow = (props) => {
     // carousel -d is added to recalculate after every resize
   }, [carousel]);
 
-  // useEffect(() => {
-  //   api('GET', 'movies').then((moviesData) => setMovies(moviesData));
-  // }, [refresh]);
+  //! This is for check!
+  const handleModal = async () => {
+    setModalOpen(!modalOpen);
+    getFavMovies(getUserSession());
+    const favs = getFavMoviesFromLocalStorage();
+    if (favs.includes(id)) {
+      setFav(true);
+    }
+  };
+  const handleFavButton = () => {
+    const userSession = getUserSession();
+    const body = { id };
+    const movie = body.id;
+    if (fav === false) {
+      movieToFav(userSession, body);
+      window.localStorage.setItem('favs', JSON.stringify(movie));
+      setFav(true);
+    } else {
+      removeMovieFromFav(userSession, movie);
+      window.localStorage.removeItem('favs', JSON.stringify(movie));
+      setFav(false);
+    }
+  };
 
   return movies?.length ? (
     <div>
+      {modalOpen && (
+        <Modal
+          id={id}
+          urlImgMovie={urlImgMovie}
+          title={title}
+          urlImgModal={urlImgModal}
+          description={movieDescription}
+          closeModal={handleModal}
+          movieRuntime={movieRuntime}
+          movieRating={movieRating}
+          setMylist={handleFavButton}
+          stateFav={fav}
+        />
+      )}
       <motion.div ref={carousel} className="carousel" whileTap={{ cursor: 'grabbing' }}>
         <motion.div
           // drag makes the motion of sliding, x means horizontal
           drag="x"
           dragConstraints={{ right: 0, left: -width }}
           className="innerCarousel">
-          {movies.map(({ _id, urlImgMovie, title, urlImgModal, description, rating, runtime }) => (
-            <motion.div className="item" key={_id}>
-              {/* <img src={movie.urlImgMovie} alt="" /> */}
-              <Card
-                id={_id}
-                urlImgMovie={urlImgMovie}
-                title={title}
-                urlImgModal={urlImgModal}
-                movieDescription={description}
-                movieRating={rating}
-                movieRuntime={runtime}
-              />
+          {movies.map((movie) => (
+            <motion.div className="item" key={movie._id} onClick={handleModal}>
+              <img src={movie.urlImgMovie} alt={movie.title} />
             </motion.div>
           ))}
         </motion.div>
